1ac1aa34b5c44730f5e78111fed5d3ad92a64224
ZenonStella <stellazenon@gmail.com>
2022-07-26 14:15:33 +0200

    integration php

---
+++ inc/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Travelcorp</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+
+<body>
+    <header class="header">
+    <nav class="nav">
+        <img class="logo" src="../assets/img/logo-travelcorp.png">
+        <div class="Continent"><a href="#">Amérique</a></div>
+        <div class="Continent"><a href="#">Europe</a></div>
+        <div class="Continent"><a href="#">Asie</a></div>
+        <div class="Continent"><a href="#">Océanie</a></div>
+        <div class="Continent"><a href="#">Afrique</a></div>
+        <img class="contact" src="../assets/img/picto contact.png">
+    </nav>
+    </header>
+    <div class="main">
